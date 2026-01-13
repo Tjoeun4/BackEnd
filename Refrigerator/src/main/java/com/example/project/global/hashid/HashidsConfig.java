e3f8d863fd6193b29b4e87770529ac8041d7e2cc
KHW <aje50101@gmail.com>
2026-01-13 15:34:13 +0900

    [Fix] Trable User 제거

--- Refrigerator/src/main/java/com/example/project/global/hashid/HashidsConfig.java
+++ Refrigerator/src/main/java/com/example/project/global/hashid/HashidsConfig.java
@@ -1,9 +1,10 @@
 package com.example.project.global.hashid;
 
 import org.hashids.Hashids;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Configuration
 public class HashidsConfig {
--- Refrigerator/src/main/java/com/example/project/security/token/TokenRepository.java
+++ Refrigerator/src/main/java/com/example/project/security/token/TokenRepository.java
@@ -10,9 +10,9 @@ import org.springframework.data.repository.query.Param;
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
   @Query(value = """
-      select t from Token t inner join TravelUser u\s
-      on t.user.id = u.id\s
-      where u.id = :id and (t.expired = false or t.revoked = false)\s
+          select t from Token t inner join Users u\s
+            on t.user.id = u.id\s
+            where u.id = :id and (t.expired = false or t.revoked = false)\s
       """)
   List<Token> findAllValidTokenByUser(@Param("id") Integer id);
   
