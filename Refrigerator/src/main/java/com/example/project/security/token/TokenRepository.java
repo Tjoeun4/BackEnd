135b49abed1f82ee5e17e2eac54251d248ccccbf
Naseoi1022 <nsi1022@naver.com>
2026-01-13 15:28:34 +0900

    base

--- Refrigerator/src/main/java/com/example/project/security/token/TokenRepository.java
+++ Refrigerator/src/main/java/com/example/project/security/token/TokenRepository.java
@@ -7,14 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
-  @Query(value = """
-      select t from Token t inner join TravelUser u\s
-      on t.user.id = u.id\s
-      where u.id = :id and (t.expired = false or t.revoked = false)\s
-      """)
-  List<Token> findAllValidTokenByUser(@Param("id") Integer id);
-  
-  Optional<Token> findByToken(String token);
-}
+	  @Query("""
+	      select t from Token t 
+	      where t.user.id = :id and t.expired = false and t.revoked = false
+	      """)
+	  List<Token> findAllValidTokenByUser(@Param("id") Integer id);
+
+	  Optional<Token> findByToken(String token);
+	}
