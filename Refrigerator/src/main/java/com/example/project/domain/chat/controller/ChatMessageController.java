096699b9e233bdb3dad5d9fe0a32c6783a73298f
xxon <163013769+Eunsseob@users.noreply.github.com>
2026-01-20 13:31:11 +0900

    [fix/#19] Hashid 관련 오류 처리

--- Refrigerator/src/main/java/com/example/project/domain/chat/controller/ChatMessageController.java
+++ Refrigerator/src/main/java/com/example/project/domain/chat/controller/ChatMessageController.java
@@ -21,10 +21,19 @@ public class ChatMessageController {
     // Flutter에서 /pub/chat/message로 메시지를 보내면 실행
     @MessageMapping("/chat/message")
     public void sendMessage(@Payload ChatMessageRequest request) {
+    	System.out.println(">>> STOMP 메시지 수신 성공! 내용: " + request.getContent());
         // 1. DB에 메시지 저장
         ChatMessageResponse response = chatService.saveMessage(request);
         
         // 2. 해당 방을 구독(/sub/chat/room/{roomId}) 중인 유저들에게 실시간 전송
         messagingTemplate.convertAndSend("/sub/chat/room/" + request.getRoomId(), response);
+    }
+    
+    @MessageMapping("/chat/message1") // 주소도 message1로 변경
+    public void sendMessage1(String message) {
+        System.out.println("================================");
+        System.out.println(">>> [테스트] STOMP 입구 도달!");
+        System.out.println(">>> 데이터 원문: " + message);
+        System.out.println("================================");
     }
 }
