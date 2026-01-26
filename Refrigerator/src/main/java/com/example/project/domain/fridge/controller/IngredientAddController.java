b217e03d20ba8a15d849ba2d3ae9757b801d26d2
KHW <aje50101@gmail.com>
2026-01-26 11:04:03 +0900

    [Fix/#29] 냉장고 관련 userid -> jwt 토큰으로 변경

--- Refrigerator/src/main/java/com/example/project/domain/fridge/controller/IngredientAddController.java
+++ Refrigerator/src/main/java/com/example/project/domain/fridge/controller/IngredientAddController.java
@@ -4,7 +4,10 @@ import com.example.project.domain.fridge.dto.*;
 import com.example.project.domain.fridge.service.IngredientAddService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import com.example.project.member.domain.Users;
+
 
 @RestController
 @RequestMapping("/api/fridge/ingredients")
@@ -16,14 +19,24 @@ public class IngredientAddController {
     // 사용자가 이름만 입력 → 매칭 결과 반환
      
     @PostMapping("/resolve")
-    public ResponseEntity<IngredientResolveResponse> resolve(@RequestBody IngredientResolveRequest req) {
+    public ResponseEntity<IngredientResolveResponse> resolve(
+            @RequestBody IngredientResolveRequest req,
+           @AuthenticationPrincipal Users userDetails
+    ) {
+        Long userId = userDetails.getUserId();
+        req.setUserId(userId);
         return ResponseEntity.ok(ingredientAddService.resolve(req));
     }
 
     // 사용자가 확정 + 상세 입력 → fridge_item 생성
      
     @PostMapping
-    public ResponseEntity<IngredientCreateResponse> create(@RequestBody IngredientCreateRequest req) {
+    public ResponseEntity<IngredientCreateResponse> create(
+            @RequestBody IngredientCreateRequest req,
+            @AuthenticationPrincipal Users userDetails
+    ) {
+        Long userId = userDetails.getUserId();
+        req.setUserId(userId);
         return ResponseEntity.ok(ingredientAddService.create(req));
     }
 }
--- Refrigerator/src/main/java/com/example/project/domain/fridge/controller/RecommendController.java
+++ Refrigerator/src/main/java/com/example/project/domain/fridge/controller/RecommendController.java
@@ -2,7 +2,9 @@ package com.example.project.domain.fridge.controller;
 
 import com.example.project.domain.fridge.dto.RecommendResponse;
 import com.example.project.domain.fridge.service.RecommendService;
+import com.example.project.member.domain.Users;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +15,10 @@ public class RecommendController {
     private final RecommendService recommendService;
 
     @GetMapping("/recommend")
-    public RecommendResponse recommend(@RequestParam("userId") Long userId) {
+    public RecommendResponse recommend(
+            @AuthenticationPrincipal Users userDetails
+    ) {
+        Long userId = userDetails.getUserId();
         return recommendService.recommend3OnlyAllowed(userId);
     }
 }
