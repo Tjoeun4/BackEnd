85deec1fa29f0af21508c2590084bfbf446084af
lealle <84366652+lealle@users.noreply.github.com>
2026-01-26 12:13:00 +0900

    Update IngredientAddService.java

--- Refrigerator/src/main/java/com/example/project/domain/fridge/service/IngredientAddService.java
+++ Refrigerator/src/main/java/com/example/project/domain/fridge/service/IngredientAddService.java
@@ -1,6 +1,7 @@
 package com.example.project.domain.fridge.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
